107cd71e7c9a6c26db17dd3540dbbdd5d8ba45e9
Niyaz Beysengulov <n.beysengulov@gmail.com>
2023-07-21 16:26:55 -0500

    moved constants into helpers, changed the version of zhk

--- fem/fieldreader.py
+++ fem/fieldreader.py
@@ -3,18 +3,6 @@ import yaml
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-import time
-
-from math import sqrt, pi
-from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size
-from matplotlib import ticker
-
-from scipy.optimize import curve_fit
-from scipy.sparse import diags
-from scipy.sparse.linalg import eigs
-from findiff import FinDiff
-from scipy.interpolate import RegularGridInterpolator
-
 from ..settings import *
 
 
@@ -22,9 +10,6 @@ ff_types = ['2Dmap', '2Dslices']
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
-
-def harmonicPotential(x, A, B, offset):
-    return  A * x**2 + B * x**4 + offset
 
 def find_max_index(arr):
     max_index = np.unravel_index(arr.argmax(), arr.shape)
@@ -134,34 +119,17 @@ def read_ff_output(filename: str, ff_type: str) -> dict:
     
     return data
 
+
 # Main Class
-
 class FieldAnalyzer():
-
-    trap_area = {
-        'x': [-0.5, 0.5],
-        'y': [-0.5, 0.5]
-    }
     
     def __init__(self, *filename_args: tuple[str, str, str]):
         for fname, attrname, dtype in filename_args:
             data = read_ff_output(fname, dtype)
             setattr(self, attrname, data)
-        
-        #with open('config.yml', 'r') as file:
-        #    self.config = yaml.safe_load(file)
-        
-    def plot_coupling_const(self, couplingConst: list, gate: str, ax=None) -> None:
-        if ax is None:
-            ax = _default_ax()
-        ax.contourf(couplingConst['xlist'], couplingConst['ylist'], couplingConst[gate], 17, cmap='RdYlBu_r', vmin=-0.03)
-        set_limits(ax, couplingConst['xlist'][0], couplingConst['xlist'][-1], couplingConst['ylist'][0], couplingConst['ylist'][-1])
 
     def potential(self, couplingConst: dict, voltages: dict, zlevel_key=None) -> tuple:
         num = len(couplingConst.keys()) - 2
-        if len(voltages) != num:
-            raise Exception(f"Incorrect length of voltage list. Please provide a list with length {num}.")
-
         nx, ny = len(couplingConst['xlist']), len(couplingConst['ylist'])
         data = np.zeros((nx, ny), dtype=np.float64)
         for (k, v) in couplingConst.items():
@@ -175,6 +143,12 @@ class FieldAnalyzer():
 
         return couplingConst['xlist'], couplingConst['ylist'], data
     
+    def plot_coupling_const(self, couplingConst: list, gate: str, ax=None) -> None:
+        if ax is None:
+            ax = _default_ax()
+        ax.contourf(couplingConst['xlist'], couplingConst['ylist'], couplingConst[gate], 17, cmap='RdYlBu_r', vmin=-0.03)
+        set_limits(ax, couplingConst['xlist'][0], couplingConst['xlist'][-1], couplingConst['ylist'][0], couplingConst['ylist'][-1])
+    
     def plot_potential2D(self, couplingConst: list, voltage_list: list, ax=None, zero_line=True, **kwargs):
         if ax is None:
             ax = _default_ax()
@@ -182,13 +156,6 @@ class FieldAnalyzer():
         im = ax.contourf(data[0], data[1], np.transpose(data[2]), 17, **kwargs)
         if zero_line:
             ax.contour(data[0], data[1], np.transpose(data[2]), [0], linestyles='dashed', colors=GRAY)
-        divider = make_axes_locatable(ax)
-        width = axes_size.AxesY(ax, aspect=1./4)
-        pad = axes_size.Fraction(0.5, width)
-        cax = divider.append_axes("right", size=width, pad=pad)
-        cb = plt.colorbar(im, cax=cax)
-        cb.locator = ticker.MaxNLocator(nbins=5)
-        cb.update_ticks()
     
     ''' TODO
     def plot_potential_3D(self, voltage_list: list, scale: str, ax=None, **kwargs):
@@ -216,4 +183,3 @@ class FieldAnalyzer():
             ax.plot(Y, -Phi[int(np.size(X)/2), :]*1e3, label=f'y_cut', **kwargs)
         ax.set_xlabel('$x$ or $y$ (um)')
         ax.set_ylabel('electrostatic potential $-\phi(x)$ (mV)')
-    
