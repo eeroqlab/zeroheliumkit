cec4d7db99619017692f96f6ff60fe28fa71d54a
Niyaz Beysengulov <n.beysengulov@gmail.com>
2023-08-25 19:01:14 -0500

    all basic geoms are collected here

---
+++ dev/basics.py
@@ -0,0 +1,190 @@
+"""
+Collection of different simple 1D/2D Geometries
+"""
+
+import numpy as np
+
+from shapely import LineString, Polygon
+
+from .core import Entity
+
+
+def Rectangle(width: float, height: float) -> Polygon:
+    """ creates a Rectangle polygon
+
+    Args:
+        width (float)
+        height (float)
+
+    Returns:
+        Polygon
+    """
+    return Polygon([(-width/2, -height/2),
+                    (-width/2, height/2),
+                    (width/2, height/2),
+                    (width/2, -height/2)])
+
+
+def Square(size: float) -> Polygon:
+    """ creates a Square polygon
+
+    Args:
+        size (float)
+
+    Returns:
+        Polygon
+    """
+    return Rectangle(size, size)
+
+
+def RegularPolygon(edge: float=None,
+                   radius: float=None,
+                   num_edges: int=6) -> Polygon:
+    """ creates a Regular polygon with specified number of edges.
+        Size of the polygon defined by radius or edge size.
+
+    Args:
+        edge (float): size of edge
+        radius (float): radius of the Regular polygon
+        num_edges (int): number of edges
+
+    Returns:
+        Polygon
+    """
+    coords = []
+    angle = 2 * np.pi / num_edges
+    if edge:
+        radius = edge/np.sin(angle/2) * 0.5
+    for i in range(num_edges):
+        xcoor = radius * np.cos(i * angle)
+        ycoor = radius * np.sin(i * angle)
+        coords.append((xcoor, ycoor))
+
+    return Polygon(coords)
+
+
+def Circle(radius: float) -> Polygon:
+    """ creates a Circle polygon
+
+    Args:
+        radius (float): radius of the circle
+
+    Returns:
+        Polygon
+    """
+    return RegularPolygon(radius=radius, num_edges=100)
+
+
+def ArcLine(centerx: float,
+            centery: float,
+            radius: float,
+            start_angle: float,
+            end_angle: float,
+            numsegments=10) -> LineString:
+    """creates a 1D arcline
+
+    Args:
+        centerx (float): center.x of arcline
+        centery (float): center.x of arcline
+        radius (float): radius of the arcline
+        start_angle (float): starting angle
+        end_angle (float): end angle
+        numsegments (int, optional): number of the segments. Defaults to 10.
+
+    Returns:
+        LineString
+    """
+    theta = np.radians(np.linspace(start_angle, end_angle, num=numsegments, endpoint=True))
+    xcoor = centerx + radius * np.cos(theta)
+    ycoor = centery + radius * np.sin(theta)
+
+    return LineString(np.column_stack([xcoor, ycoor]))
+
+
+def Meander(length: float,
+            radius: float,
+            direction: float,
+            num_segments: int=100) -> LineString:
+    """ creates a 1D full Meander line
+
+    Args:
+        length (float): length of the straight section
+        radius (float): radius of the round section
+        direction (float): rotates the meander by given value in the end
+        num_segments (int, optional): number of segments in round section
+                                      Defaults to 100
+
+    Returns:
+        LineString
+    """
+
+    coord_init = [(0,0), (0,length/2)]
+    meander_entity = Entity()
+    meander_entity.add_line(LineString(coord_init))
+    meander_entity.add_line(ArcLine(radius, 0, radius, 180, 0, num_segments))
+    meander_entity.add_line(LineString([(0,0), (0,-length)]))
+    meander_entity.add_line(ArcLine(radius, 0, radius, 180, 360, num_segments))
+    meander_entity.add_line(LineString([(0,0), (0,length/2)]))
+    meander_entity.rotate(direction, origin=(0,0))
+
+    return meander_entity.skeletone
+
+
+def MeanderHalf(length: float,
+                radius: float,
+                direction: float,
+                num_segments: int=100) -> LineString:
+    """ creates a 1D half Meander line
+
+    Args:
+        length (float): length of the straight section
+        radius (float): radius of the round section
+        direction (float): rotates the meander by given value in the end
+        num_segments (int, optional): number of segments in round section
+                                      Defaults to 100
+
+    Returns:
+        LineString
+    """
+
+    coord_init = [(0,0), (0,length/2)]
+    meander_entity = Entity()
+    meander_entity.add_line(LineString(coord_init))
+    meander_entity.add_line(ArcLine(radius, 0, radius, 180, 0, num_segments))
+    meander_entity.add_line(LineString([(0,0), (0,-length/2)]))
+    meander_entity.rotate(direction, origin=(0,0))
+
+    return meander_entity.skeletone
+
+
+def PinchGate(arm_w: float,
+              arm_l: float,
+              length: float,
+              width: float) -> Polygon:
+    """ creates a pinch gate polygon
+
+    Args:
+        arm_w (float): arm width
+        arm_l (float): arm length
+        length (float): length of the pinch gate
+        width (float): width of the pinch gate
+
+    Returns:
+        Polygon
+    """
+
+    pts = [(-arm_w/2, arm_w/2),
+            (arm_l - length/32, arm_w/2),
+            (arm_l, length/8),
+            (arm_l, 7*length/16),
+            (arm_l + width/3, length/2),
+            (arm_l + width, length/2),
+            (arm_l + width, -length/2),
+            (arm_l + width/3, -length/2),
+            (arm_l, -7*length/16),
+            (arm_l, -length/8),
+            (arm_l - length/32, -arm_w/2),
+            (-arm_w/2, -arm_w/2)
+            ]
+
+    return Polygon(pts)
