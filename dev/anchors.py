1002a19aec10c5638221046d846980071556dc0a
Niyaz Beysengulov <n.beysengulov@gmail.com>
2023-08-25 19:36:19 -0500

    cleaned up the code

--- dev/anchors.py
+++ dev/anchors.py
@@ -1,74 +1,100 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import copy
-
 from math import fmod
-from tabulate import tabulate
-from shapely import Point
-from shapely import affinity, set_precision
+import numpy as np
 
-from ..settings import *
+from tabulate import tabulate
+from shapely import Point, affinity, set_precision
+
+from ..settings import GRID_SIZE, BLACK, RED
 from ..helpers.plotting import default_ax
-from .functions import modFMOD
+
+
+def modFMOD(angle: float | int) ->float:
+    """ modified modulo calculations for angles in degrees
+        lower branch always has negative sign
+
+    Args:
+        angle (float):
+
+    Returns:
+        angle: angle modulo 360
+    """
+
+    if np.abs(angle) % 360 == 180:
+        return 180
+
+    if np.abs(angle) % 360 < 180:
+        return fmod(angle, 360)
+
+    if np.sign(angle) > 0:
+        return angle % 360 - 360
+
+    return angle % 360
 
 
 class Anchor():
+    """ Anchor class, which has three attributes:
+        1. coordinates (tuple | Point)
+        2. orientation/direction
+        3. label
+    """
+
     __slots__ = "point", "direction", "label"
-    
+
     def __init__(self, point: Point | tuple=Point(), direction: float=0, label: str=None):
         if not isinstance(point, Point):
             coords = np.array(point).squeeze()
-            self.point = Point(coords)
+            self.point = set_precision(Point(coords), grid_size=GRID_SIZE)
         else:
-            self.point = point
+            self.point = set_precision(point, grid_size=GRID_SIZE)
         self.direction = modFMOD(direction)
         self.label = label
-    
+
     @property
     def x(self):
         return self.point.x
-    
+
     @property
     def y(self):
         return self.point.y
-    
+
     @property
     def coords(self):
         return (self.point.x, self.point.y)
-    
+
     @coords.setter
     def coords(self, xy: tuple):
-        self.point = Point(*xy)
-    
+        self.point = set_precision(Point(*xy), grid_size=GRID_SIZE)
+
     @property
     def properties(self):
-        print(tabulate([["label", "coords", "direction"], 
+        print(tabulate([["label", "coords", "direction"],
                         [self.label, self.coords, self.direction]]))
-    
+
     def rename(self, newlabel: str) -> None:
         self.label = newlabel
-    
+
     def rotate(self, angle: float, origin: tuple=(0,0)):
         """ Rotates the point by 'angle' around 'origin'"""
         point_upd = affinity.rotate(self.point, angle, origin)
         self.point = set_precision(point_upd, grid_size=GRID_SIZE)
-        if self.direction != None:
+        if self.direction is not None:
             self.direction += angle
-        
+
         self.direction = modFMOD(self.direction)
-    
+
     def move(self, xoff: float=0, yoff: float=0):
         point_upd = affinity.translate(self.point, xoff=xoff, yoff=yoff)
         self.point = set_precision(point_upd, grid_size=GRID_SIZE)
-    
+
     def scale(self, xfact: float=1.0, yfact: float=1.0, origin: tuple=(0,0)):
         point_upd = affinity.scale(self.point, xfact=xfact, yfact=yfact, origin=origin)
         self.point = set_precision(point_upd, grid_size=GRID_SIZE)
-        if self.direction != None:
+        if self.direction is not None:
             if yfact < 0:
                 #rotation around x-axis
                 self.direction = (-1) * self.direction
-            
+
             if xfact < 0:
                 #rotation around y_axis
                 self.direction = 180 - self.direction
@@ -80,49 +106,56 @@ class Anchor():
             self.scale(-1, 1)
         elif aroundaxis not in ['x', 'y', None]:
             raise ValueError("choose 'x', 'y or None for mirror arguments")
-        
+
         if aroundaxis and update_label:
             self.label = update_label
-    
+
     def plot(self, ax=None, color: str=None, draw_direction: bool=True):
         if ax is None:
             ax = default_ax()
-        
+
         coords = (self.point.x, self.point.y)
 
         x1, x2 = ax.get_xlim()
         y1, y2 = ax.get_ylim()
-        if self.direction != None and draw_direction:
+        if self.direction is not None and draw_direction:
             arrow_length = min(np.abs(x2-x1), np.abs(y2-y1)) * 0.08
-            arrow_kwargs = dict(arrowstyle='->', linewidth=2, color=RED)
-            ax.annotate('', 
-                        xy=(coords[0] + arrow_length*np.cos(self.direction*np.pi/180), coords[1] + arrow_length*np.sin(self.direction*np.pi/180)), 
-                        xytext=(coords[0], coords[1]), 
+            arrow_kwargs = {"arrowstyle": '->', "linewidth": 2, "color": RED}
+            ax.annotate('',
+                        xy=(coords[0] + arrow_length*np.cos(self.direction*np.pi/180),
+                            coords[1] + arrow_length*np.sin(self.direction*np.pi/180)),
+                        xytext=(coords[0], coords[1]),
                         arrowprops=arrow_kwargs)
         ax.annotate(self.label,
                     coords,
                     color=BLACK,
-                    clip_on=True, 
-                    bbox=dict(facecolor='white', edgecolor=color, boxstyle='round', alpha=0.7))
+                    clip_on=True,
+                    bbox={"facecolor": 'white',
+                          "edgecolor": color,
+                          "boxstyle": 'round',
+                          "alpha": 0.7})
         ax.plot(coords[0], coords[1], linestyle="", marker=".", color=color, alpha=1, zorder=1e9)
 
 
 class MultiAnchor():
+    """ MultiAnchor class, which has one attribute:
+        1. multipoint - a list of Anchors
+    """
+
     __slots__ = "multipoint"
-    
+
     def __init__(self, multipoint: list=[]):
         self.multipoint = multipoint
-    
+
     @property
     def labels(self) -> list:
         return [p.label for p in self.multipoint]
-    
+
     def label_exist(self, label: str) -> bool:
         if label in self.labels:
             return True
-        else:
-            False
-    
+        return False
+
     def copy(self):
         """ Returns  deepcopy of the class """
         return copy.deepcopy(self.multipoint)
@@ -130,7 +163,7 @@ class MultiAnchor():
     def rotate(self, angle: float, origin: tuple=(0,0)):
         for p in self.multipoint:
             p.rotate(angle, origin)
-    
+
     def move(self, xoff: float=0, yoff: float=0):
         for p in self.multipoint:
             p.move(xoff, yoff)
@@ -138,7 +171,7 @@ class MultiAnchor():
     def scale(self, xfact: float=1.0, yfact: float=1.0, origin: tuple=(0,0)):
         for p in self.multipoint:
             p.scale(xfact, yfact, origin)
-    
+
     def mirror(self, aroundaxis: str=None, update_labels: bool=False, keep_original: bool=False):
         if not keep_original:
             if not update_labels:
@@ -150,27 +183,25 @@ class MultiAnchor():
         else:
             original = self.copy()
             for p in self.multipoint:
-                    p.mirror(aroundaxis, p.label + "_m")
+                p.mirror(aroundaxis, p.label + "_m")
             self.multipoint = self.multipoint + original
 
-       
     def __point(self, label: str):
         idx = self.labels.index(label)
         return self.multipoint[idx]
 
     def point(self, labels: list[str]):
-        if type(labels) is list:
+        if isinstance(labels, list):
             return [self.__point(l) for l in labels]
-        else:
-            return self.__point(labels)
-    
+        return self.__point(labels)
+
     def remove(self, labels: list | str):
         if isinstance(labels, str):
             labels = [labels]
         S1 = set(self.point(labels))
         S2 = set(self.multipoint)
         self.multipoint = list(S2.difference(S1))
-    
+
     def modify(self, label: str, new_name: str=None, new_xy: tuple=None, new_direction: float=None):
         if new_xy:
             self.__point(label).coords = new_xy
@@ -178,15 +209,15 @@ class MultiAnchor():
             self.__point(label).direction = new_direction
         if new_name:
             self.__point(label).label = new_name
-    
+
     def add(self, points: list[Anchor] | Anchor=[]):
         if not isinstance(points, list):
             points = [points]
         for p in points:
             if self.label_exist(p.label):
-                raise ValueError(f"point label {p} is already exist in MultiAnchor. Choose different label name.")
+                raise ValueError(f"""point label {p} already exists in MultiAnchor.
+                                 Choose different label name.""")
         self.multipoint += points
-
 
     def plot(self, ax=None, color: str=None, draw_direction: bool=True):
         for p in self.multipoint:
