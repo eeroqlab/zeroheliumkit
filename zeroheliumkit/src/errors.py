c0f17ffcc3ab5a074978a18621959d6633248596
Niyaz Beysengulov <n.beysengulov@gmail.com>
2024-03-01 08:04:01 -0600

    update docstring

--- zeroheliumkit/src/errors.py
+++ zeroheliumkit/src/errors.py
@@ -5,13 +5,34 @@ class TopologyError(Exception):
     """A geometry topologically incorrect."""
 
 class FreefemError(Exception):
+    """
+    Exception raised for errors related to FreeFEM.
+
+    Attributes:
+        message -- explanation of the error
+    """
     pass
 
 class RouteError(Exception):
+    """
+    Exception raised for errors related to routing.
+
+    Attributes:
+        message -- explanation of the error
+    """
     pass
 
 class GeometryError(Exception):
+    """
+    Exception raised for errors related to geometry calculations.
+
+    Attributes:
+        message -- explanation of the error
+    """
     pass
 
 class WrongSizeError(Exception):
+    """
+    Exception raised when an operation is performed with an incorrect size.
+    """
     pass
