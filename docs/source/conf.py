30115d71acb33b9c8e2ac31b65b47db6db47ac2a
Niyaz <n.beysengulov@gmail.com>
2025-08-17 22:42:13 -0500

    bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,8 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../fem'))
 
 project = 'ZeroHeliumKit'
 copyright = '2025, EeroQ'
