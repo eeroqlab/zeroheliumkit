98e953cd4810dae868b472eed9f1794a538b4851
Niyaz <n.beysengulov@gmail.com>
2025-08-18 20:23:04 -0500

    bugfix readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@ print(os.path.dirname(__file__))
 print(os.path.join(os.path.dirname(__file__), '../..'))
 print(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+print(os.listdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))))
 # sys.path.insert(0, os.path.abspath('../src'))
 # sys.path.insert(0, os.path.abspath('../fem'))
 
